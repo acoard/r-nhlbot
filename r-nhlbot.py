a5da6e4a5724b0956002a5f58f853276850f2911
acoard <arcoard+github@gmail.com>
2013-08-07 18:48:52 -0700

    Added makePosts().

--- r-nhlbot.py
+++ r-nhlbot.py
@@ -1,11 +1,11 @@
 #reddit bot for /r/nhl
-
 import praw
+import time
 
-user_agent = ("Bot for aggregating hockey team related subreddit top posts. v0.5 /u/acoard")
-r = praw.Reddit(user_agent=user_agent)
+r = praw.Reddit("User Agent: NHLBot for /r/hockey. v1.0 created by /u/acoard")
+nb = "NHLBot"
 
-class nhlBot:
+class NHLBot:
 	hockey_subs = ['canucks', "anaheimducks", "calgaryflames", "losangeleskings", 
 			"coyotes", "sanjosesharks", "hawks", "coloradoavalanche", "dallasstars", 
 			"wildhockey", "predators", "stlouisblues", "winnipegjets", "canes", "bluejackets", 
@@ -13,21 +13,35 @@ class nhlBot:
 			"sabres", "detroitredwings", "floridapanthers", "habs", "ottawasenators", "tampabaylightning", "leafs"]
 	hockey_subs_TEMP = ['canucks', 'leafs', 'flyers']
 
-	def getHockeyPosts(self):
+	def getTopPosts(self):
 		numberOfSubmissions = 1
 		#Put submissions in a dictionary so that they can be linked to each subreddit.
 		top_submissions = dict() 
 		for subreddit in self.hockey_subs_TEMP: #DEV only
 			top_submissions[subreddit] = [x for x in r.get_subreddit(subreddit).get_top_from_week(limit=numberOfSubmissions)][0]
+			time.sleep(5) #29 teams, 5 second rest == 145sec wait.
 		return top_submissions
 
 	def formatPosts(self, posts):
-		#Takes getHockeyPosts() for input.
+		#Takes getTopPosts() for input.
 		message = "Top posts for each hockey team's subreddit.\n"
 		for i in posts:
 			message += str(i) + ": " + "[" + str(posts[i]).split(' :: ')[1] +"](" + str(posts[i].short_link) + ")" \
 			+ " with a karma of " + str(posts[i]).split(' ::')[0] + "\n"
 		return message
 
-nhlBot = nhlBot()
-posts = nhlBot.getHockeyPosts()
+	def run(self):
+		pass
+
+	def makePost(self, subreddit, title):
+		if not r.is_logged_in():
+			print "Bot is not logged in.  Please login by typing r.login()."
+		else:
+			r.submit(subreddit, title, text=formy)
+
+	def editSidebar(self, subreddit):
+		pass
+
+NHLBot = NHLBot()
+posts = NHLBot.getTopPosts()
+formy = NHLBot.formatPosts(posts)
